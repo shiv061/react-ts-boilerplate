38bdf7b17d2fc93fe2835702ce106aef2d2c80b0
shiv061 <shivansh0200@gmail.com>
2024-10-13 09:34:33 +0530

    update: package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello, React with TypeScript!</h1>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl">Hello, React with TypeScript!</h1>
     </div>
   );
 };
